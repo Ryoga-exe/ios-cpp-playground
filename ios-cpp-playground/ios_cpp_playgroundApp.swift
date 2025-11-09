35d16c8ab88136dd9e0e09ef248c2b6da4d0135d
Ryoga-exe <git@ryoga.dev>
2025-11-10 02:07:13 +0900

    feat: initialize project

---
+++ ios-cpp-playground/ios_cpp_playgroundApp.swift
@@ -0,0 +1,15 @@
+//
+//  ios_cpp_playgroundApp.swift
+//  ios-cpp-playground
+//
+
+import SwiftUI
+
+@main
+struct ios_cpp_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
