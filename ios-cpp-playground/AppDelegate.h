82545749acd9d08b57397475b6ef252b89d32ad2
Ryoga-exe <git@ryoga.dev>
2025-11-10 02:36:36 +0900

    feat: initialize project with Game template

---
+++ ios-cpp-playground/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  ios-cpp-playground
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
